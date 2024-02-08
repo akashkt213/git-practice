d6c03f82e082f12f5cbf9920047731a754377c2c
Akash <akashkt213@gmail.com>
2024-02-08 10:36:13 +0530

    stashed files restored

--- datopic.js
+++ datopic.js
@@ -6,4 +6,10 @@ console.log("Atta market and The great India mall");
 
 console.log("The grand venice mall");
 
-console.log("Room");
+console.log("Learning Stashing");
+console.log("Creating some line to stash those lines");
+console.log("These lines are not added and committed yet.");
+console.log(
+  "so if we try to pull the changes done by someone in the remote we have to first commit these changes and then we can pull those"
+);
+console.log("but to dont want to commit now so we stash it.");
