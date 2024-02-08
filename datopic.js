acde40a1d1d0a2ef6337fb6bc18957500415192a
Akash <akashkt213@gmail.com>
2024-02-08 10:53:27 +0530

    datopic changes

---
+++ datopic.js
@@ -0,0 +1,20 @@
+console.log(
+  "The office is 1 kms from the Vertigo hotel and we walk to the office."
+);
+
+console.log("Went to dlf mall");
+
+console.log("Atta market and The great India mall");
+
+console.log("The grand venice mall");
+
+console.log("Learning Stashing");
+console.log("Creating some line to stash those lines");
+console.log("These lines are not added and committed yet.");
+console.log(
+  "so if we try to pull the changes done by someone in the remote we have to first commit these changes and then we can pull those"
+);
+console.log("but to dont want to commit now so we stash it.");
+
+console.log("trying stashing for the second time");
+console.log("just another line");
